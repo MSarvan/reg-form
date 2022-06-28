777dabd91ebe764edaa7eee2bcdbd536763c52e8
Saravanan M <sarvan.m20@gmail.com>
2022-06-28 18:44:06 +0530

    UI updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,32 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  
-
   return (
     <div className="App">
       <div>
+        <h2>Register</h2>
         <form action="">
-          <input type="text" placeholder='Enter your Name*'/>
+          <input type="text" placeholder="Enter your Name*" required/>
           <br />
-          <input type="text" placeholder='Enter your Mail Id*'/>
+          <input type="text" placeholder="Enter your Mail Id*" required/>
           <br />
-          <input type="text" placeholder='Enter your Mobile no.'/>
+          <input type="text" placeholder="Enter your Mobile no." />
           <br />
-          <input type="text" placeholder='Country'/>
+          <input type="text" placeholder="Country" />
           <br />
-          <input type="text" placeholder='City'/>
+          <input type="text" placeholder="City" />
           <br />
-          <input type="text" placeholder='State'/>
+          <input type="text" placeholder="State" />
           <br />
-          <input type="text" placeholder='Message'/>
-          <br /><br />
+          <input type="text" placeholder="Message" />
+          <br />
+          <br />
           <button>Submit</button>
         </form>
-
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Components/Formsubmission.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Formsubmission = () => {
+  return (
+    <div>Formsubmission</div>
+  )
+}
