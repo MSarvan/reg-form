12f18a9d30fdd275469ada2b5871209d00338fdb
Saravanan M <sarvan.m20@gmail.com>
2022-06-28 18:03:00 +0530

    UI updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,31 @@
 import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <div>
+        <form action="">
+          <input type="text" placeholder='Enter your Name*'/>
+          <br />
+          <input type="text" placeholder='Enter your Mail Id*'/>
+          <br />
+          <input type="text" placeholder='Enter your Mobile no.'/>
+          <br />
+          <input type="text" placeholder='Country'/>
+          <br />
+          <input type="text" placeholder='City'/>
+          <br />
+          <input type="text" placeholder='State'/>
+          <br />
+          <input type="text" placeholder='Message'/>
+          <br /><br />
+          <button>Submit</button>
+        </form>
+
+      </div>
     </div>
   )
 }
